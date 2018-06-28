0b6ea5c9699950be392a5c0aa75ba27b939285a6
Harley Milhomes <harleymilhomes@gmail.com>
2018-06-28 11:55:08 -0300

    python 14 exec

--- python-3.5/Exerc14.py
+++ python-3.5/Exerc14.py
@@ -3,6 +3,6 @@ print("VOLUME DA ESFERA NO CUBO")
 r = int(input('Raio: >> '))
 a = int(input('Aresta: >> '))
 
-print("Volume da esfera no cubo: ", (math.pi * math.pow(a, 3)) / 6)
+print("Volume da esfera no cubo: ", (4 * math.pi * math.pow(r, 3)) / 3)
 
 print("Fim!!!")
